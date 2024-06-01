6a7aae11a8e4aa5f074570c3a99270801743be02
Simon Sure <info@simonsure.com>
2024-06-01 09:12:34 +0200

    [setup] adding cloudbuild configuration
    Signed-off-by: Simon Sure <info@simonsure.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      <h1 className="text-4xl font-bold text-blue-500">Hello, UBAP Project!</h1>
     </div>
   );
 }
