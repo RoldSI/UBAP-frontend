641a6dd2dbb8dbab10d7ef3fbb5a51dc3ee60bb5
Simon Sure <info@simonsure.com>
2024-06-01 19:34:55 +0200

    remove persona input
    Signed-off-by: Simon Sure <info@simonsure.com>

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ function App() {
   return (
     <div className="h-screen p-4 bg-gray-100">
       <div className="grid grid-cols-8 grid-rows-8 gap-4 h-full">
-        <div className="col-span-6 row-span-7">
+        <div className="col-span-6 row-span-8">
           <Content/>
         </div>
         <div className="bg-white rounded-lg shadow p-4 col-span-2 row-span-8">
           <Summary/>
         </div>
-        <div className="bg-white rounded-lg shadow p-4 col-span-6 row-span-1">
+        {/* <div className="bg-white rounded-lg shadow p-4 col-span-6 row-span-1">
           <Persona persona={persona} setPersona={setPersona}/>
-        </div>
+        </div> */}
       </div>
     </div>
   );
