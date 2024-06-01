4a5d79722359613468c819f6dc4a4230d8a6d311
Simon Sure <info@simonsure.com>
2024-06-01 10:51:31 +0200

    [ui] adding high-level ui layout
    Signed-off-by: Simon Sure <info@simonsure.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Summary from "./components/summary";
+import Persona from "./components/persona";
+import Content from "./content";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, UBAP Project!</h1>
+    <div className="h-screen p-4 bg-gray-100">
+      <div className="grid grid-cols-8 grid-rows-8 gap-4 h-full">
+        <div className="col-span-6 row-span-7">
+          <Content/>
+        </div>
+        <div className="bg-white rounded-lg shadow p-4 col-span-2 row-span-8">
+          <Summary/>
+        </div>
+        <div className="bg-white rounded-lg shadow p-4 col-span-6 row-span-1">
+          <Persona/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/persona.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Persona() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Persona</h1>
+      <p>You will specify the persona here.</p>
+    </div>
+  );
+}
+
+export default Persona;
